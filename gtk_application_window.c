384c36be3a2db5791dcafb4da71688caeade77a6
Alvaro Paz da Silva <alanpasi@gmail.com>
2025-04-08 19:33:38 -0300

    Refactor GTK app and add text entry widget
    Improve code formatting with consistent 4-space indentation, fix
    the activate() function signature by removing unused parameter,
    and reorder widget creation code for better organization.

--- gtk_application_window.c
+++ gtk_application_window.c
@@ -5,80 +5,84 @@ static void click_cb(GtkButton *button, gpointer user_data) {
 }
 
 static void click_quit(GtkButton *button, GtkApplicationWindow *window) {
-  g_print("Saindo...\n");
-  gtk_window_destroy (GTK_WINDOW (window));
+    g_print("Saindo...\n");
+    gtk_window_destroy (GTK_WINDOW (window));
 }
 
-static void activate(GApplication *app, gpointer user_data) {
+static void activate(GApplication *app) {
 
-  GtkWidget *window;
-  GtkWidget *label1;
-  GtkWidget *separator1;
-  GtkWidget *button1;
-  GtkWidget *button2;
-  GtkWidget *button3;
+    GtkWidget *window;
+    GtkWidget *label1;
+    GtkWidget *separator1;
+    GtkWidget *button1;
+    GtkWidget *button2;
+    GtkWidget *button3;
+    GtkWidget *entry1;
 
-  GtkBuilder *builder;
-  GtkCssProvider *provider;
+    GtkBuilder *builder;
+    GtkCssProvider *provider;
 
-  GdkDisplay *display;
+    GdkDisplay *display;
 
-  /* Construct a GtkBuilder instance and load our UI description */
-  builder = gtk_builder_new_from_file("window.ui");
+    /* Construct a GtkBuilder instance and load our UI description */
+    builder = gtk_builder_new_from_file("window.ui");
 
-  window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
-  gtk_window_set_application(GTK_WINDOW(window), GTK_APPLICATION(app));
-  gtk_window_set_default_size(GTK_WINDOW(window), 300, 200);
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
+    gtk_window_set_application(GTK_WINDOW(window), GTK_APPLICATION(app));
+    gtk_window_set_default_size(GTK_WINDOW(window), 300, 200);
 
-  /* Alterar cor da fonte com CSS */
-  provider = gtk_css_provider_new();
-  gtk_css_provider_load_from_path(provider, "style.css");
+    /* Alterar cor da fonte com CSS */
+    provider = gtk_css_provider_new();
+    gtk_css_provider_load_from_path(provider, "style.css");
 
-  /* Aplica o provedor de CSS à tela */
-  display = gdk_display_get_default();
-  gtk_style_context_add_provider_for_display(
-      display, GTK_STYLE_PROVIDER (provider),
-      GTK_STYLE_PROVIDER_PRIORITY_USER);
+    /* Aplica o provedor de CSS à tela */
+    display = gdk_display_get_default();
+    gtk_style_context_add_provider_for_display(
+        display, GTK_STYLE_PROVIDER (provider),
+        GTK_STYLE_PROVIDER_PRIORITY_USER);
 
-  /* Cria um GtkSeparator */
-  separator1 = GTK_WIDGET(gtk_builder_get_object(builder, "separator1"));
-  gtk_widget_add_css_class(GTK_WIDGET(separator1), "separator");
+    /* Cria um GtkLabel */
+    label1 = GTK_WIDGET(gtk_builder_get_object(builder, "label1"));
+    gtk_label_set_text(GTK_LABEL(label1), "Label 1...");
 
-  /* Cria um GtkLabel */
-  label1 = GTK_WIDGET(gtk_builder_get_object(builder, "label1"));
-  gtk_label_set_text(GTK_LABEL(label1), "Novo label...");
+    /* Cria um GtkSeparator */
+    separator1 = GTK_WIDGET(gtk_builder_get_object(builder, "separator1"));
+    gtk_widget_add_css_class(GTK_WIDGET(separator1), "separator");
 
-   /* Cria um GtkButton */
-  button1 = GTK_WIDGET(gtk_builder_get_object(builder, "button1"));
-  g_signal_connect(button1, "clicked", G_CALLBACK(click_cb), "Button 1");
-  gtk_widget_add_css_class(GTK_WIDGET(button1), "bt");
+    /* Cria um GtkButton */
+    button1 = GTK_WIDGET(gtk_builder_get_object(builder, "button1"));
+    g_signal_connect(button1, "clicked", G_CALLBACK(click_cb), "Button 1");
+    gtk_widget_add_css_class(GTK_WIDGET(button1), "bt");
 
-  button2 = GTK_WIDGET(gtk_builder_get_object(builder, "button2"));
-  g_signal_connect(button2, "clicked", G_CALLBACK(click_cb), "Button 2");
-  gtk_widget_add_css_class(GTK_WIDGET(button2), "bt");
+    button2 = GTK_WIDGET(gtk_builder_get_object(builder, "button2"));
+    g_signal_connect(button2, "clicked", G_CALLBACK(click_cb), "Button 2");
+    gtk_widget_add_css_class(GTK_WIDGET(button2), "bt");
 
-  button3 = GTK_WIDGET(gtk_builder_get_object (builder, "quit"));
-  g_signal_connect(button3, "clicked", G_CALLBACK(click_quit), window);
-  gtk_widget_add_css_class(GTK_WIDGET(button3), "bt_quit");
+    button3 = GTK_WIDGET(gtk_builder_get_object (builder, "quit"));
+    g_signal_connect(button3, "clicked", G_CALLBACK(click_quit), window);
+    gtk_widget_add_css_class(GTK_WIDGET(button3), "bt_quit");
 
-  g_object_unref(builder);
+    entry1 = GTK_WIDGET(gtk_builder_get_object(builder, "entry1"));
+    gtk_widget_add_css_class(GTK_WIDGET(entry1), "entry");
 
-  gtk_window_present(GTK_WINDOW (window));
+    g_object_unref(builder);
+
+    gtk_window_present(GTK_WINDOW (window));
 }
 
 int main (int argc, char *argv[]) {
 
-  GtkApplication *app;
-  int status;
+    GtkApplication *app;
+    gint status;
 
-  /* Create a GtkApplication instance. */
-  app = gtk_application_new("com.alanpasi.gtk_application_window",
-                             G_APPLICATION_DEFAULT_FLAGS);
-  g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
-  /* Run the application. */
-  status = g_application_run(G_APPLICATION(app), argc, argv);
+    /* Create a GtkApplication instance. */
+    app = gtk_application_new("com.alanpasi.gtk_application_window",
+                                G_APPLICATION_DEFAULT_FLAGS);
+    g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+    /* Run the application. */
+    status = g_application_run(G_APPLICATION(app), argc, argv);
 
-  g_object_unref(app);
+    g_object_unref(app);
 
-  return status;
+    return status;
 }
